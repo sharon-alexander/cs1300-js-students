3e35163b561f80b3dc8c0ed8b479b1fa7a3e96e6
sharon-alexander <sharon_alexander@brown.edu>
2020-10-31 11:43:23 -0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=Q9tGsot1A7ZCtm7SU4YTo9pr9PmUa3eoVRk4DkThuAI";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,43 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      console.log(request.data); //request.responseText 
+      handleResponse(request.response); //added 
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+const handleResponse = (response) => {
+  const jsonified = JSON.parse(response);
+  const plants = jsonified.data;
+  console.log(plants); //will show what the parsed plants looks like 
+
+  const plantsAfter1755 = plants.filter((arrayItem) => {
+    return arrayItem.year > 1755;
+
+  });
+
+
+  for (const plant of plantsAfter1755) {
+    turnPlantIntoHTML(plant);
+
+  }
+}
+
+const turnPlantIntoHTML = (plant) => {
+
+  const plantDiv = document.createElement("div"); //you can set anything onto this div element now
+  plantDiv.innerText = plant.common_name;
+  const commonContainerDiv = document.getElementById("common-container");
+  commonContainerDiv.appendChild(plantDiv);
+
+  const sciDiv = document.createElement("div"); //you can set anything onto this div element now
+  sciDiv.innerText = plant.scientific_name;
+  const scientificContainerDiv = document.getElementById("scientific-container");
+  scientificContainerDiv.appendChild(sciDiv);
+  
+
+
+
+}
